655b4aed848885d7f8175bd58e7230fc976fbadb
Pram-Nayak <nayakpratham2011@gmail.com>
2025-02-03 17:45:39 +0530

    updated commit

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -5,8 +5,6 @@ from data_input import data_in
 import numpy as np
 import pickle
 
-
-
 def load_models():
     file_name = "models/model_file.p"
     with open(file_name, 'rb') as pickled:
@@ -15,14 +13,12 @@ def load_models():
     return model
 
 app = Flask(__name__)
-@app.route('/predict', methods=['GET'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    # stub input features
     request_json = request.get_json()
     x = request_json['input']
-    #print(x)
-    x_in = np.array(x).reshape(1,-1)
-    # load model
+    x_in = np.array(x).reshape(1, -1)
     model = load_models()
     prediction = model.predict(x_in)[0]
     response = json.dumps({'response': prediction})
